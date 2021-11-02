911f478998c47e1c660a1548f57b24b434d40886
Joseph Holten <joseph@holten.com>
2021-11-02 13:01:22 +0100

    started work on selecting unique id

--- main.py
+++ main.py
@@ -20,62 +20,58 @@ def extract_session_key(message: str) -> str:
     key_end = message.find('"', key_start+1)
     return message[key_start+1:key_end]
 
-
 status = bw_status()
 
 session_key = ""
 
 if status['status'] == 'unlocked':
     session_key = os.environ.get('BW_SESSION')
+
 else:
-    # if unauthenticated or locked you need the password
+    # try to find session key in keyctl 
+    output = subprocess.run(["keyctl", "request", "user", "bw_session"], capture_output=True, text=True)
 
-    # password = run_cmd(['rofi', '-dmenu', '-p', 'Master Password', '-password', '-lines' ,'0'])
-    # password = password.rstrip('\n')
-    password = '7N"}g<wux,?w2]fS'
-    output = None
+    # session key available (i.e. defacto unlocked)
+    if output.stderr == '' and output.stdout.rstrip("\n").isdigit():  
+        key_id = output.stdout.rstrip("\n")
+        output = subprocess.run(["keyctl", "print", key_id], capture_output=True, text=True)
 
-    if status['status'] == 'unauthenticated':
-        # code = subprocess.run(['rofi', '-dmenu','-p', 'YubiKey 2FA code', '-password', '-lines' ,'0'], capture_output=True, text=True).stdout.rstrip("\n")
-        code = input("Provide 2FA: ")
-        output = subprocess.run(['bw', 'login', 'joseph@holten.com', password, '--method', '3', '--code', code], capture_output=True, text=True)
+        session_key = output.stdout.rstrip("\n")
         
-        if not output.stdout.startswith('You are logged in!'):   # failed at logging in
-            print("Error logging in: ", output, file=sys.stderr)
-            exit(1)
+        print("Success getting session_key from keyctl")
 
-    if status['status'] == 'locked':
-        output = subprocess.run(["keyctl", "request", "user", "bw_session"])
+    # no session key saved with keyctl (i.e. locked or unauthenticated)
+    else:
+        # definitely need password
+        password = subprocess.run(['rofi', '-dmenu', '-p', 'Master Password', '-password', '-lines' ,'0'], capture_output=True, text=True).stdout
+        password = password.rstrip('\n')
 
-        if output.stderr == '' and output.stdout.rstrip("\n").isdigit():  # session key available
-            key_id = output.stdout.rstrip("\n")
-            output = subprocess.run(["keyctl", "print", key_id], capture_output=True, text=True)
-            session_key = output.stdout.rstrip("\n")
+        output = None
 
-        if output.stderr == 'request_key: Required key not available':  # session auto locked
+        if status['status'] == 'unauthenticated':
+            code = subprocess.run(['rofi', '-dmenu','-p', 'YubiKey 2FA code', '-password', '-lines' ,'0'], capture_output=True, text=True).stdout.rstrip("\n")
+            output = subprocess.run(['bw', 'login', 'joseph@holten.com', password, '--method', '3', '--code', code], capture_output=True, text=True)
+            
+            if not output.stdout.startswith('You are logged in!'):   # failed at logging in
+                print("Error logging in: ", output, file=sys.stderr)
+                exit(1)
+
+        elif status['status'] == 'locked':
             output = subprocess.run(["bw", "unlock", password]) 
 
-    else:
-        assert false, "Unhandled status"
+        else:
+            assert False, f"Unhandled status {status}"
 
-    session_key = extract_session_key(output.stdout)
-
-    output = subprocess.run(["keyctl", "add", "user", "bw_session", session_key, "@u"], capture_output=True, text=True)
-    key_id = output.stdout.rstrip("\n")
-    output = subprocess.run(["keyctl", "timeout", key_id, AUTO_LOCK], capture_output=True, text=True)
-
-            
-            
+        session_key = extract_session_key(output.stdout)
 
 
+# register session_key with keyctl
+output = subprocess.run(["keyctl", "add", "user", "bw_session", session_key, "@u"], capture_output=True, text=True)
+key_id = output.stdout.rstrip("\n")
+output = subprocess.run(["keyctl", "timeout", key_id, str(AUTO_LOCK)], capture_output=True, text=True)
 
-# MAIN PART
-
-
-
-
-# try to reschedule the logout
-output = subprocess.run(["keyctl", "request", "user", "bw_logout"])
+# deschedule old logout
+output = subprocess.run(["keyctl", "request", "user", "bw_logout"], capture_output=True, text=True)
 if output.stdout.rstrip("\n").isdigit():  # have gotten a key_id
     # remove pending logout
     key_id = output.stdout.rstrip("\n")
@@ -84,18 +80,31 @@ if output.stdout.rstrip("\n").isdigit():  # have gotten a key_id
 
     output = subprocess.run(["atrm", job_id], capture_output=True, text=True)
 
-    if output.stderr or output.stdout:
-        print("Error rescheduling 'bw logout': invalid jobid.\nkeyctl error message: ", output, file=sys.stderr)
-        exit(1)
 
-if not output.stdout.rstrip("\n").isdigit() or output.stderr != "request_key: Required key not available":  # some error, that is not that the job has already been performed
+elif output.stderr != "request_key: Required key not available\n":  # some error, that is not that the job has already been performed
     print("Error rescheduling 'bw logout': couldn't get job_id from keyctl.\nkeyctl error message: ", output, file=sys.stderr)
     exit(1)
+            
+# MAIN PART
+
+output = subprocess.run(["bw", "list", "items", "--session", session_key], capture_output=True, text=True)
+items = json.loads(
+
+if (sys.argc == 1):   # init call to program
+    # list all items by name
+
+
+
+
+output = subprocess.run(f'bw list items --session {session_key}', capture_output=True, text=True, shell=True)
+
+items = json.loads(output.stdout)
+pprint(items)
+
+# END MAIN
 
 # schedule new logout
-output = os.popen(f'echo "bw logout" | at now + {AUTO_LOGOUT} minutes')
-job_id = output.readline().rstrip("\n")
+output = subprocess.run(f'echo "bw logout" | at now + {AUTO_LOGOUT} minutes', capture_output=True, text=True, shell=True)
+job_id = output.stderr.split("\n")[1].split()[1]
 output = subprocess.run(["keyctl", "add", "user", "bw_logout", job_id, "@u"], capture_output=True, text=True)
-output = subprocess.run(["keyctl", "timeout", AUTO_LOGOUT*60], capture_output=True, text=True)
-
-
+output = subprocess.run(["keyctl", "timeout", str(AUTO_LOGOUT*60)], capture_output=True, text=True)
